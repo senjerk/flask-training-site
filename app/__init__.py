00c327e6b6e166231fa9c08620fee7c79f822a80
senjerk <oleg.bagryanov@gmail.com>
2023-04-27 20:44:50 +0300

    add some cool features

--- app/__init__.py
+++ app/__init__.py
@@ -3,3 +3,5 @@ from flask import Flask
 app = Flask(__name__, template_folder='templates')
 
 from app import routes
+
+# app.run()
--- app/routes.py
+++ app/routes.py
@@ -2,8 +2,33 @@
 from flask import render_template
 from app import app
 
+
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Miguel'}
     return render_template('index.html', title='Home', user=user)
+
+
+@app.route('/chat')
+def chat():
+    user = {'username': 'Adam Jensen'}
+    posts = [
+        {
+            'author': {'username': 'Jeff'},
+            'body': 'My name is Jeff.'
+        },
+        {
+            'author': {'username': 'Guard'},
+            'body': 'I used to be an adventurer like you, then I took an arrow in the knee.'
+        },
+        {
+            'author': {'username': 'Anon'},
+            'body': 'sup guys, <$#fds> in the house!'
+        },
+        {
+            'author': {'username': 'test_name'},
+            'body': 'Sample text. Lorem ipsum.'
+        }
+    ]
+    return render_template('chat.html', title='Home', user=user, posts=posts)
