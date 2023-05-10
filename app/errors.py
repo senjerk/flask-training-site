9ad14efa9bd4d5dcfa61599c2682bc127ac9869f
senjerk <oleg.bagryanov@gmail.com>
2023-05-10 11:25:47 +0300

    added log system, some cool tests, follower system

--- app/errors.py
+++ app/errors.py
@@ -1,7 +1,8 @@
 from flask import render_template
 from app import app, db
 import logging
-from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -11,9 +12,6 @@ if not app.debug:
         secure = None
         if app.config['MAIL_USE_TLS']:
             secure = ()
-            print(app.config['MAIL_SERVER'], app.config['MAIL_PORT'])
-            print(app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
-            print(auth)
         mail_handler = SMTPHandler(
             mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
             fromaddr=app.config['MAIL_USERNAME'],
@@ -21,6 +19,18 @@ if not app.debug:
             credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
 
 
 @app.errorhandler(404)
